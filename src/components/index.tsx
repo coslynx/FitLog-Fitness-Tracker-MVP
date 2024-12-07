10c7239440c6c11362627790287403b2a998c085
CosLynx AI <drixisgod@gmail.com>
2024-12-07 18:27:16 +0530

    generated file: `src/components/index.tsx`

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as AuthButton } from './AuthButton';
+export { default as GoalCard } from './GoalCard';
+export { default as GoalForm } from './GoalForm';
+export { default as ProgressChart } from './ProgressChart';
+export { default as ShareLink } from './ShareLink';
